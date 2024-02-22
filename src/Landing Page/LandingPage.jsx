34c2e38070c1f6478f7de19fedc8ae4a74abc665
Faheem Ul Hassan Aziz <seebiz.gpt0623.dev56@gmail.com>
2024-02-22 19:10:40 +0500

    navbar is ok now

--- src/Landing Page/LandingPage.jsx
+++ src/Landing Page/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header/Header";
+import Navbar from "./Navbar/Navbar";
 
 function LandingPage() {
   return (
     <>
       <Header></Header>
+      <Navbar />
     </>
   );
 }
