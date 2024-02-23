2f5d4a41bb7b48c1a43e3e83b2c43ef398ace5b5
Faheem Ul Hassan Aziz <seebiz.gpt0623.dev56@gmail.com>
2024-02-23 18:30:56 +0500

    the header code is optimised

--- src/Landing Page/Header/Header.jsx
+++ src/Landing Page/Header/Header.jsx
@@ -13,70 +13,71 @@ function Header() {
   return (
     <>
       <Flex
-        width="100vw"
+        // width="100vw"
+        justifyContent="center"
         height="40px"
         border="0px, 0px, 1px, 0px"
         bgColor="#3A0CA3"
+        gap="900px"
         // justifyContent="space-around"
       >
-        <Icon
-          as={FontAwesomeIcon}
-          icon={faLocationDot}
-          color="white"
-          width="12px"
-          height="16px"
-          mt="9px"
-          ml="100px"
-          border="1.2px"
-        />
-        <CustomText
-          width="194px"
-          height="9px"
-          mt="6px"
-          ml="6px"
-          color="#FFFFFF"
-        >
-          Rezilla, 18 Grattan St, Brooklyn
-        </CustomText>
-        <PhoneIcon
-          width="16px"
-          height="16px"
-          mt="12px"
-          ml="815px"
-          color="#FFFFFF"
-        />
-        <CustomText
-          width="109px"
-          height="9px"
-          mt="9px"
-          ml="6px"
-          color="#FFFFFF"
-        >
-          +1 206-214-2298
-        </CustomText>
-        <EmailIcon
-          width="16px"
-          height="16px"
-          mt="12px"
-          ml="11px"
-          border="1.2px"
-          color="white"
-        />
-        <CustomText
-          width="137px"
-          height="9px"
-          mt="9px"
-          ml="6px"
-          color="#FFFFFF"
-          mr="100px"
-        >
-          Support@gmail.com
-        </CustomText>
-      </Flex>
-      <Flex>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
+        <Flex>
+          <Icon
+            as={FontAwesomeIcon}
+            icon={faLocationDot}
+            color="white"
+            width="12px"
+            height="16px"
+            mt="9px"
+            // ml="10px"
+            border="1.2px"
+          />
+          <CustomText
+            width="194px"
+            height="9px"
+            mt="6px"
+            ml="6px"
+            color="#FFFFFF"
+          >
+            Rezilla, 18 Grattan St, Brooklyn
+          </CustomText>
+        </Flex>
+        <Flex>
+          <PhoneIcon
+            width="16px"
+            height="16px"
+            mt="12px"
+            // ml="815px"
+            color="#FFFFFF"
+          />
+          <CustomText
+            width="109px"
+            height="9px"
+            mt="9px"
+            ml="6px"
+            color="#FFFFFF"
+          >
+            +1 206-214-2298
+          </CustomText>
+          <EmailIcon
+            width="16px"
+            height="16px"
+            mt="12px"
+            ml="11px"
+            border="1.2px"
+            color="white"
+          />
+          <CustomText
+            width="137px"
+            height="9px"
+            mt="9px"
+            ml="6px"
+            color="#FFFFFF"
+            // mr="100px"
+          >
+            Support@gmail.com
+          </CustomText>
+        </Flex>
       </Flex>
     </>
   );
